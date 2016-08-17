3eefffee92463f3e10a628f5e94fdc8dfebd7a11
Kexin Zhang <kxin.zhang@gmail.com>
2016-08-17 13:32:18 -0400

    changed contact links, fixed some form errors

--- sendForm.php
+++ sendForm.php
@@ -1,10 +1,11 @@
 <?php
+
     if (isset($_POST["submit"])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
         $human = intval($_POST['human']);
-        $from = 'Contact Form';
+        $from = 'From:' . $email;
         $to = 'scno@gmail.com';
         $subject = 'Message from Contact Form';
 
@@ -30,9 +31,9 @@
         }
 
 // If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+if (!isset($errName) &&  !isset($errEmail) && !isset($errMessage) && !isset($errHuman)) {
     if (mail ($to, $subject, $body, $from)) {
-        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+        $result='<div class="alert alert-success">Thank You! We will be in touch</div>';
     } else {
         $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
     }
@@ -53,12 +54,12 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
     </head>
 
     <body>
-        <!-- <link href = "css/bootstrap.min.css" rel = "stylesheet">
+<!--         <link href = "css/bootstrap.min.css" rel = "stylesheet">
         <link href = "css/styles.css" rel = "stylesheet"> -->
 
         <div class = "navbar navbar-default navbar-static-top">
-            <div class = "container">
-                <a href="#" class="pull-left"><img src="pics/navbarlogo3.png" width="250" height="70"></a>
+            <div class = "navigation">
+                <a href="index.html" class="logo"><img class="img-responsive" src="pics/navbarlogo3.png" width="250" height="70"></a>
 
                 <button class = "navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
                     <span class = "icon-bar"></span>
@@ -72,61 +73,64 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
                         <li class = "dropdown">
                             <a href = "#" class = "dropdown-toggle" data-toggle="dropdown">About SCNO <b class = "caret"></b></a>
                             <ul class = "dropdown-menu">
-                                <li><a href = "#">What We Do</a></li>
-                                <li><a href = "#">Executive Board</a></li>
-                                <li><a href = "#">Our Clients</a></li>
+                                <li><a href = "we.html">What We Do</a></li>
+                                <li class="active"><a href = "members.html">Our Members</a></li>
+                                <li><a href = "clients.html">Our Clients</a></li>
                             </ul>
                         </li>
+                        <li><a href = "calendar.html">Calendar</a></li>
                         <li><a href = "apply.html">Apply</a></li>
-                        <li class = "active"><a href = "contact.html">Contact Us</a></li>
+                        <li><a href = "sendForm.php">Contact Us</a></li>
                         <li><a href = "https://www.facebook.com/GTSCNO/" target="_blank">
-                            <img src = "pics/fblogo.jpg" width="35" height="35">
+                            <img class="img-respnsive" src = "pics/fblogo.jpg" width="35" height="35">
                         </a></li>
                     </ul>
                 </div>
             </div>
         </div>
 
-        <form class="form-horizontal" role="form" method="post" action="index.php">
-            <div class="form-group">
-                <label for="name" class="col-sm-2 control-label">Name</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
-                    <?php echo "<p class='text-danger'>$errName</p>";?>
+        <div class="container" style="padding-top: 3rem;">
+            <form class="form-horizontal" role="form" method="post" action="<?PHP echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <div class="form-group">
+                    <label for="name" class="col-sm-2 control-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php if (isset($_POST['name'])) { echo htmlspecialchars($_POST['name']); } ?>">
+                        <?php if (isset($errName)) { echo "<p class='text-danger'>$errName</p>"; } ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="email" class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
-                    <?php echo "<p class='text-danger'>$errEmail</p>";?>
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php if (isset($_POST['email'])) { echo htmlspecialchars($_POST['email']); } ?>">
+                        <?php if (isset($errEmail)) { echo "<p class='text-danger'>$errEmail</p>"; } ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="message" class="col-sm-2 control-label">Message</label>
-                <div class="col-sm-10">
-                    <textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
-                    <?php echo "<p class='text-danger'>$errMessage</p>";?>
+                <div class="form-group">
+                    <label for="message" class="col-sm-2 control-label">Message</label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" rows="4" name="message"><?php if (isset($_POST['message'])) { echo htmlspecialchars($_POST['message']); }?></textarea>
+                        <?php if (isset($errMessage)) { echo "<p class='text-danger'>$errMessage</p>"; } ?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
-                    <?php echo "<p class='text-danger'>$errHuman</p>";?>
+                <div class="form-group">
+                    <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+                        <?php if (isset($errHuman)) { echo "<p class='text-danger'>$errHuman</p>" ; }?>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-10 col-sm-offset-2">
-                    <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-10 col-sm-offset-2">
-                    <?php echo $result; ?>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <?php if (isset($result)) { echo $result; } ?>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
 
 
         <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
