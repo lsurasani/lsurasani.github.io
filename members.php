54b981535a30a4eaed52d0b0339150b86be36c97
Kexin Zhang <kxin.zhang@gmail.com>
2017-08-02 19:09:46 -0400

    more exec pictures

--- members.php
+++ members.php
@@ -75,14 +75,14 @@
                     $vivek->major="Industrial Engineering";
                     $vivek->imgsrc="pics/vivek.jpg";
 
-                    $kexin = new Member();
-                    $kexin->name = 'Kexin Zhang';
-                    $kexin->linkedin = 'https://www.linkedin.com/in/kexinzhang1/';
-                    $kexin->title = 'VP of Marketing and Recruitment';
-                    $kexin->major = 'Industrial Engineering';
-                    $kexin->year = '3rd';
-                    $kexin->imgsrc = 'pics/kexin.jpeg';
-
+                    $dani = new Member();
+                    $dani->name="Dani Gresge";
+                    $dani->linkedin = 'https://www.linkedin.com/in/danielle-gresge-a69803106';
+                    $dani->title = 'VP of External Affairs';
+                    $dani->year = '3rd';
+                    $dani->major = 'Industrial Engineering';
+                    $dani->imgsrc="pics/dani.jpg";
+ 
                     $lynn = new Member();
                     $lynn->name = "Lynn You";
                     $lynn->linkedin = 'https://www.linkedin.com/in/lynnyou';
@@ -91,7 +91,39 @@
                     $lynn->year = '3rd';
                     $lynn->imgsrc = "pics/lynn.jpg";
 
-                    $execMembers = array($lekha, $vivek, $kexin, $lynn);
+                    $ronak = new Member();
+                    $ronak->name = 'Ronak Mukhopadhyay';
+                    $ronak->linkedin = '';
+                    $ronak->title = 'VP of Projects';
+                    $ronak->year = '4th';
+                    $ronak->major = 'Industrial Engineering';
+                    $ronak->imgsrc = 'pics/ronak.png';
+
+                    $rohil = new Member();
+                    $rohil->name = 'Rohil Shah';
+                    $rohil->linkedin = '';
+                    $rohil->title = 'Director of Projects';
+                    $rohil->year = '3rd';
+                    $rohil->major = 'Industrial Engineering';
+                    $rohil->imgsrc = 'pics/rohil.png';
+
+                    $kexin = new Member();
+                    $kexin->name = 'Kexin Zhang';
+                    $kexin->linkedin = 'https://www.linkedin.com/in/kexinzhang1/';
+                    $kexin->title = 'VP of Marketing and Recruitment';
+                    $kexin->major = 'Industrial Engineering';
+                    $kexin->year = '3rd';
+                    $kexin->imgsrc = 'pics/kexin.jpeg';
+
+                    $ellen = new Member();
+                    $ellen->name = 'Ellen Pao';
+                    $ellen->linkedin = 'https://www.linkedin.com/in/ellentpao/';
+                    $ellen->title = 'Director of Marketing';
+                    $ellen->major = 'Business and Computer Science';
+                    $ellen->year = '3rd';
+                    $ellen->imgsrc = 'pics/ellen.jpg';
+
+                    $execMembers = array($lekha, $vivek, $dani, $lynn, $ronak, $rohil, $kexin, $ellen);
 
                     echo '<div class="ui cards">';
 
@@ -99,7 +131,7 @@
                         echo '<div class="card">';
                         echo '<div class="image"><img src="' . $execMember->imgsrc . '"></div>';
                         echo '<div class="content">';
-                        echo $execMember->name . '<br>' . $execMember->year . 'Year, ' . $execMember->major . '<br>' . $execMember->title;
+                        echo $execMember->name . '<br>' . $execMember->year . ' Year, ' . $execMember->major . '<br>' . $execMember->title;
                         echo '</div>';
                         echo '</div>';
                     }
