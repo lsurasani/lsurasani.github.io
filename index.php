9e2c4bb0bb7d50d8f7b4fb28c72e43cadf893263
Kexin Zhang <kxin.zhang@gmail.com>
2017-08-02 23:58:35 -0400

    links

--- index.php
+++ index.php
@@ -25,19 +25,19 @@
                             <h3 class = "text-center text-uppercase text-primary">Empowering Communities</h3><br>
                             <img src = "pics/nonprofit.jpg" class = "center-block"><br>
                             <h6 class = "text-center">We consult for nonprofit organizations in Atlanta, solving different kinds of problems.</h6>
-                            <a href = "we.html" class = "ui primary basic button">Get Consulted</a>
+                            <a href = "we.php" class = "ui primary basic button">Get Consulted</a>
                         </div>
                         <div class="column">
                             <h3 class = "text-center text-uppercase text-primary">Developing Leaders</h3><br>
                             <img src = "pics/student.jpg" class = "center-block"><br>
                             <h6 class = "text-center">We have the most passionate students from a variety of majors to create innovative solutions.</h6>
-                            <a href = "apply.html" class = "ui primary basic button">Apply Now</a>
+                            <a href = "apply.php" class = "ui primary basic button">Apply Now</a>
                         </div>
                         <div class="column">
                             <h3 class = "text-center text-uppercase text-primary">Delivering Results</h3><br>
                             <img src = "pics/results.jpg" class = "center-block"><br>
                             <h6 class = "text-center">We give our clients a presentation and a detailed report of our recommendations.</h6>
-                             <a href = "clients.html" class = "ui primary basic button">Our Clients</a>
+                             <a href = "clients.php" class = "ui primary basic button">Our Clients</a>
                         </div>
                     </div>
                 </div>
