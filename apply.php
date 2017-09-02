e9cf976359f3a51548580f5393914b9350f3ae0d
kexin-zhang <kxin.zhang@gmail.com>
2017-09-02 19:02:13 -0400

    took out application

--- apply.php
+++ apply.php
@@ -17,8 +17,7 @@
                 <div class="ui horizontal divider">
                     <h1>Apply</h1>
                 </div>
-                <p>We are now accepting applications for Fall 2017. To be considered, please apply using the form below. We're also hosting <a href="https://www.facebook.com/events/1983116585301117" target="_blank">a mixer for prospective members</a> on August 31st at 6pm in DM Smith Room 105! </p>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdXu-meHb5oj8VM4ECI62L3UY8TGrn-Fv1zjQP-qWl7G5TiMg/viewform?embedded=true" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                <p>We are no longer accepting applications for Fall 2017. If you are interested in joining SCNO, please check back in January about our Spring 2018 application process!</p>
             </div>
 
             <?php include "footer.html"; ?>
