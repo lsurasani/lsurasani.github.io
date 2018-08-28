89eadb3d6e369777e900d5047fba3b57766287b8
epao3 <ellenpao@gatech.edu>
2018-08-28 09:14:01 -0400

    Update apply.php

--- apply.php
+++ apply.php
@@ -21,6 +21,7 @@
                 <p><a href = https://docs.google.com/forms/d/e/1FAIpQLSckZEjcVeF_VHGCWSLNtJiLPW5NpPSnCCGYS5aHLqAdp6nuYQ/viewform> click here</a></p>
                 <p> Come join us at the CULC starbucks for a chat! </p>
                 <p> August 23, 24, or 28 from 10am - 1pm</p>
+                <p> </p>
                 <p> Attend our informational mixer this Wednesday, August 29th from 6pm-7pm at the Instructional Center room 205</p>
             </div>
 
