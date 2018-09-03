d42b8e3d625591db03c53a51f5076cd13eaab5af
epao3 <ellenpao@gatech.edu>
2018-09-03 15:33:44 -0400

    Update apply.php

--- apply.php
+++ apply.php
@@ -17,11 +17,7 @@
                 <div class="ui horizontal divider">
                     <h1>Info</h1>
                 </div>
-                <p>Apply for an analyst position for our Fall 2018 Client Projects!</p>
-                <p><a href = https://docs.google.com/forms/d/e/1FAIpQLSckZEjcVeF_VHGCWSLNtJiLPW5NpPSnCCGYS5aHLqAdp6nuYQ/viewform> click here</a></p>
-                <p> Come join us at the CULC starbucks for a chat! August 23, 24, or 28 from 10am - 1pm</p>
-                <p> </p>
-                <p> Attend our informational mixer this Wednesday, August 29th from 6pm-7pm at the Instructional Center room 205</p>
+                <p> Our Fall 2018 applications have closed </p>
             </div>
 
             <?php include "footer.html"; ?>
