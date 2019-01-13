825db6194e8a7b48e0ebfde96c89b7fb24afaf1f
Margarita Groisman <mg@lawn-128-61-40-168.lawn.gatech.edu>
2019-01-13 14:02:23 -0500

    fixed spacing

--- apply.php
+++ apply.php
@@ -19,7 +19,6 @@
                 </div>
                 <p> We will be having a coffee chat at the Culc Starbucks on Thursday, January 17th from 11AM-1PM. Drop by between classes to ask any questions about our organization and hear our members' experiences from the Fall to see if SCNO is the right fit for you before applications close on Friday. </p>
                 <br></br>
-                <br></br>
                 <p> Interested in being a key player at SCNO? Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. We are currently looking to fill analyst and senior analyst positions for the Spring.<a href = "https://goo.gl/forms/ejHmLHt0RUAT5Nju1" target="_blank"> Apply here</a> by January 18th. </p>
             </div>
 
