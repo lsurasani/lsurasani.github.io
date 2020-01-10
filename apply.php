a2d29fb7824f0de26eb9fcea3f68e1a8ddde4794
scnogt <scnogt@gmail.com>
2020-01-09 21:23:45 -0500

    Update apply.php

--- apply.php
+++ apply.php
@@ -19,7 +19,7 @@
                 </div>
                 <br></br>
             <p> Interested in being a key player at SCNO? </p>
-            <p> Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. We are currently looking to fill analyst, senior analyst, and team lead positions for the Fall. </p>
+            <p> Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. We are currently looking to fill analyst, senior analyst, and team lead positions for the Spring. </p>
             
             <p> <a href = "https://forms.gle/PRJK1UJp43J4nSjw6" target="_blank"> Apply here</a> by January 17. </p>
             </div>
