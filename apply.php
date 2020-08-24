4ebe979e5b488bbba3158056eb7044859d9a7468
scnogt <scnogt@gmail.com>
2020-08-24 00:27:06 -0400

    Update apply.php

--- apply.php
+++ apply.php
@@ -19,9 +19,9 @@
                 </div>
                 <br></br>
             <p> Interested in being a key player at SCNO? </p>
-            <p> Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. We are currently looking to fill business analyst and team lead positions. </p>
-            <p> Applications are now live to join SCNO for the Fall 2020 semester! </p>
-            <p> <a href = "https://forms.gle/b2cjfryK96GidLaz8" target="_blank"> Apply here</a> by August 23. </p> 
+            <p> Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. </p>
+            <p> Applications to join SCNO for this semester are currently closed. Please check back here again before the start of the Spring semester to apply to join us! </p>
+          <!--  <p> <a href = "https://forms.gle/b2cjfryK96GidLaz8" target="_blank"> Apply here</a> by August 23. </p> -->
             </div>
 
             <?php include "footer.html"; ?>
