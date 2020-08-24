d7dccfed206320ee953dac4bdc71df26250683cd
scnogt <scnogt@gmail.com>
2020-08-24 00:28:34 -0400

    Update apply.php

--- apply.php
+++ apply.php
@@ -20,7 +20,7 @@
                 <br></br>
             <p> Interested in being a key player at SCNO? </p>
             <p> Our mission is to support non-profits by providing high-quality, impact-driven strategic consulting so they can operate more effectively. SCNO also fosters member growth through corporate networking and leadership development events, and hands-on experience. SCNO is open to all students and majors who are passionate about serving others and taking their professional skills to the next level. </p>
-            <p> Applications to join SCNO for this semester are currently closed. Please check back here again before the start of the Spring semester to apply to join us! </p>
+            <p> Applications to join SCNO for the Fall semester are currently closed. Please check back here again before the start of the Spring semester to apply to join us! </p>
           <!--  <p> <a href = "https://forms.gle/b2cjfryK96GidLaz8" target="_blank"> Apply here</a> by August 23. </p> -->
             </div>
 
